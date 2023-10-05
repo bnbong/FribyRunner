61d2f8d690ad4393ba3a6acc2dd27e5b5ff9b7c0
bnbong <bbbong9@gmail.com>
2023-10-05 16:34:33 +0900

    mobile 만보계 application update - 실제 모바일 기기 연동 및 작동 확인

--- mobile/src/main/java/com/example/fribyrunner/MainActivity.kt
+++ mobile/src/main/java/com/example/fribyrunner/MainActivity.kt
@@ -1,48 +1,76 @@
 package com.example.fribyrunner
 
 import android.content.Context
+import android.Manifest
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
-    private var stepSensor: Sensor? = null
-    private var stepCount = 0
+    private var stepCountSensor: Sensor? = null
+    private lateinit var stepCountView: TextView
+    private lateinit var resetButton: Button
+    private var currentSteps = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-    }
+        stepCountView = findViewById(R.id.stepCountView)
+        resetButton = findViewById(R.id.resetButton)
 
-    override fun onResume() {
-        super.onResume()
-        stepSensor?.also {
-            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACTIVITY_RECOGNITION
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            requestPermissions(arrayOf(Manifest.permission.ACTIVITY_RECOGNITION), 0)
+        }
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+
+        stepCountSensor?.let {
+            Toast.makeText(this, "No Step Sensor", Toast.LENGTH_SHORT).show()
+        }
+
+        resetButton.setOnClickListener {
+            currentSteps = 0
+            stepCountView.text = currentSteps.toString()
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-        sensorManager.unregisterListener(this)
+    override fun onStart() {
+        super.onStart()
+        stepCountSensor?.let {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
-
-    override fun onSensorChanged(event: SensorEvent?) {
-
-        if (event?.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
-            stepCount = event.values[0].toInt()
-            val tvStepCount: TextView = findViewById(R.id.tvStepCount)
-            tvStepCount.text = "Steps: $stepCount"
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_STEP_DETECTOR) {
+            if (event.values[0] == 1.0f) {
+                currentSteps++
+                stepCountView.text = currentSteps.toString()
+            }
         }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do nothing for now
     }
 }
