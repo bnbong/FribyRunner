342c1f2e4a71bbc06397027c7557b4c2115f551a
bnbong <bbbong9@gmail.com>
2023-10-05 15:12:41 +0900

    만보기 api test

--- mobile/src/main/java/com/example/fribyrunner/MainActivity.kt
+++ mobile/src/main/java/com/example/fribyrunner/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.fribyrunner
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var stepSensor: Sensor? = null
+    private var stepCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        stepSensor?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        if (event?.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
+            stepCount = event.values[0].toInt()
+            val tvStepCount: TextView = findViewById(R.id.tvStepCount)
+            tvStepCount.text = "Steps: $stepCount"
+        }
     }
 }
